00d2e61215c3e4b9bc9466e623d003ff9640efd6
Justice Okpor <99841261+Justo32@users.noreply.github.com>
2023-07-08 20:42:27 +0100

    update

--- main.js
+++ main.js
@@ -82,6 +82,71 @@
   
 
 
+// const form = document.getElementById('wageForm');
+
+// form.addEventListener('submit', function(event) {
+//     event.preventDefault();
+//     // employee type
+//     const EMPLOYEE_TYPE_FULL_TIME = 'full-time';
+//     const EMPLOYEE_TYPE_PART_TIME = 'part-time';
+//     // employee required hours
+//     const FULL_TIME_HOURS = 150;
+//     const PART_TIME_HOURS = 80;
+//     // employee rate in terms of wage for full time
+//     const FULL_TIME_HOURLY_RATE = 11.75;
+//     const FULL_TIME_OVERTIME_RATE = 13.75;
+//     const FULL_TIME_BANK_HOLIDAY_RATE = 23.5;
+//     const FULL_TIME_WAKING_NIGHT_RATE = 111.625;
+//     const FULL_TIME_SLEEP_RATE = 30;
+//     const FULL_TIME_EXTRA_SLEEP_RATE = 60;
+//     // employee rate in terms of wage for part time
+//     const PART_TIME_HOURLY_RATE = 11.75;
+//     const PART_TIME_OVERTIME_RATE = 13.75;
+//     const PART_TIME_BANK_HOLIDAY_RATE = 23.5;
+//     const PART_TIME_SLEEP_RATE = 30;
+//     const PART_TIME_WAKING_NIGHT_RATE = 111.625;
+//     const PART_TIME_EXTRA_SLEEP_RATE = 60;
+//     // get users input
+//     const employeeType = document.getElementById('employeeType').value;
+//     const hoursWorked = document.getElementById('hoursWorked').value;
+//     const holidayHour = document.getElementById('holidayHour').value || 0 ;
+//     const sleepTimes = document.getElementById('sleepTimes').value || 0;
+//     const extraSleepTimes = document.getElementById('extraSleepTimes').value || 0;
+//     const wakingNightTimes = document.getElementById('wakingNightTimes').value || 0;
+//     // initialize wage variable
+//     let regularPay = 0;
+//     // calculation logic base on employee type for full time
+//     if (employeeType === EMPLOYEE_TYPE_FULL_TIME) {
+//         if (hoursWorked <= FULL_TIME_HOURS) {
+//             regularPay =int(FULL_TIME_HOURLY_RATE * hoursWorked;
+//         } else if (hoursWorked > FULL_TIME_HOURS) {
+//             const overtimeHours = hoursWorked - FULL_TIME_HOURS;
+//             regularPay = FULL_TIME_HOURLY_RATE * FULL_TIME_HOURS + FULL_TIME_OVERTIME_RATE * overtimeHours;
+//         }
+//         // logic for full time
+//         regularPay += FULL_TIME_BANK_HOLIDAY_RATE * holidayHour;
+//         regularPay += FULL_TIME_SLEEP_RATE * sleepTimes;
+//         regularPay += FULL_TIME_EXTRA_SLEEP_RATE * extraSleepTimes;
+//         regularPay += FULL_TIME_WAKING_NIGHT_RATE * wakingNightTimes;
+//         // calculation logic base on employee type for part time
+//     } else if (employeeType === EMPLOYEE_TYPE_PART_TIME) {
+//         if (hoursWorked <= PART_TIME_HOURS) {
+//             regularPay = PART_TIME_HOURLY_RATE * hoursWorked;
+//         } else if (hoursWorked > PART_TIME_HOURS) 
+//         {
+//             const overtimeHours = hoursWorked - PART_TIME_HOURS;
+//             regularPay = PART_TIME_HOURLY_RATE * PART_TIME_HOURS + PART_TIME_OVERTIME_RATE * overtimeHours;
+//         }
+
+//         regularPay += PART_TIME_BANK_HOLIDAY_RATE * holidayHour;
+//         regularPay += PART_TIME_SLEEP_RATE * sleepTimes;
+//         regularPay += PART_TIME_EXTRA_SLEEP_RATE * extraSleepTimes;
+//         regularPay += PART_TIME_WAKING_NIGHT_RATE * wakingNightTimes;
+//     }
+//     // display result
+//     document.getElementById('result').textContent = `Your total wage for this month is: ${regularPay}`;
+// });
+
 const form = document.getElementById('wageForm');
 
 form.addEventListener('submit', function(event) {
@@ -108,11 +173,11 @@ form.addEventListener('submit', function(event) {
     const PART_TIME_EXTRA_SLEEP_RATE = 60;
     // get users input
     const employeeType = document.getElementById('employeeType').value;
-    const hoursWorked = document.getElementById('hoursWorked').value;
-    const holidayHour = document.getElementById('holidayHour').value || 0 ;
-    const sleepTimes = document.getElementById('sleepTimes').value || 0;
-    const extraSleepTimes = document.getElementById('extraSleepTimes').value || 0;
-    const wakingNightTimes = document.getElementById('wakingNightTimes').value || 0;
+    const hoursWorked = parseFloat(document.getElementById('hoursWorked').value);
+    const holidayHour = parseFloat(document.getElementById('holidayHour').value) || 0;
+    const sleepTimes = parseFloat(document.getElementById('sleepTimes').value) || 0;
+    const extraSleepTimes = parseFloat(document.getElementById('extraSleepTimes').value) || 0;
+    const wakingNightTimes = parseFloat(document.getElementById('wakingNightTimes').value) || 0;
     // initialize wage variable
     let regularPay = 0;
     // calculation logic base on employee type for full time
@@ -132,8 +197,7 @@ form.addEventListener('submit', function(event) {
     } else if (employeeType === EMPLOYEE_TYPE_PART_TIME) {
         if (hoursWorked <= PART_TIME_HOURS) {
             regularPay = PART_TIME_HOURLY_RATE * hoursWorked;
-        } else if (hoursWorked > PART_TIME_HOURS) 
-        {
+        } else if (hoursWorked > PART_TIME_HOURS) {
             const overtimeHours = hoursWorked - PART_TIME_HOURS;
             regularPay = PART_TIME_HOURLY_RATE * PART_TIME_HOURS + PART_TIME_OVERTIME_RATE * overtimeHours;
         }
@@ -142,8 +206,9 @@ form.addEventListener('submit', function(event) {
         regularPay += PART_TIME_SLEEP_RATE * sleepTimes;
         regularPay += PART_TIME_EXTRA_SLEEP_RATE * extraSleepTimes;
         regularPay += PART_TIME_WAKING_NIGHT_RATE * wakingNightTimes;
+         // Round up the result
+         regularPay = Math.ceil(regularPay);
     }
     // display result
     document.getElementById('result').textContent = `Your total wage for this month is: ${regularPay}`;
 });
-
